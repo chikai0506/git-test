c65e02f8bcbaa872f1eee3823075d45f7286cf83
ck <ck@163.com>
2022-06-08 23:41:33 +0800

    idea git first commit

---
+++ src/main/java/com/zh/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zh.git;
+
+/**
+ * @author ck
+ * @packageName com.zh.git
+ * @className: GitTest
+ * @description:
+ * @date 2022/6/8 23:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
