506a2c216e9a64045df24456fbfa9cce9e3d2f0e
ck <ck@163.com>
2022-06-08 23:50:25 +0800

    second commit

--- src/main/java/com/zh/git/GitTest.java
+++ src/main/java/com/zh/git/GitTest.java
@@ -10,5 +10,6 @@ package com.zh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test2");
     }
 }
